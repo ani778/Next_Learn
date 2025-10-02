da820e1ccc27910fbef6815f82733660ccf2ea2f
ahovhann_elcomp <ahovhann@estee.com>
2025-10-02 18:20:31 +0400

    fixing createClient

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,11 +1,11 @@
-import {createClient} from '@vercel/postgres';
+import { createClient } from '@vercel/postgres';
 import { sql } from '@vercel/postgres'
 import {unstable_noStore as noStore} from 'next/cache'
 
 export async  function connectToDB(){
-    const client = createClient();
-    await client.connect();
-
+    const client = createClient({
+        connectionString: process.env.POSTGRES_URL_NON_POOLING,
+    });
     try {
         if (client){
             return client
@@ -19,7 +19,6 @@ export async  function connectToDB(){
 export async function getPosts() {
     try {
         noStore();
-
         const data = await sql`SELECT * FROM posts`;
         return data.rows
     } catch (err) {
